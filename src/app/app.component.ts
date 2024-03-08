661a5bc78bde714271ea5c26e89e98dfbb1e12d6
audre <audrey.kindermans@outlook.be>
2024-03-08 15:56:00 +0100

    creation route navbar a propos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BudgetsTransactionsComponent } from './components/pages/budgets-transactions/budgets-transactions.component';
+import { NavbarComponent } from './components/components/navbar/navbar.component';
+import { AProposComponent } from './components/pages/a-propos/a-propos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, BudgetsTransactionsComponent, AProposComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/components/structure-budgets/structure-budgets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-budgets',
+  standalone: true,
+  imports: [],
+  templateUrl: './structure-budgets.component.html',
+  styleUrl: './structure-budgets.component.css'
+})
+export class StructureBudgetsComponent {
+
+}
