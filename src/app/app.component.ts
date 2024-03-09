261659c317ebcc91b10e7504590f1a25e868f656
audre <audrey.kindermans@outlook.be>
2024-03-09 13:06:10 +0100

    tailwind.config.js->couleurs et polices (adaptation navbar et footer) + routes fonctionnelles navbar (a propos et contact) + creation component contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/components/navbar/navbar.component';
 import { FooterComponent } from './components/components/footer/footer.component';
 import { AProposComponent } from './components/pages/a-propos/a-propos.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, AProposComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AProposComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AProposComponent } from './components/pages/a-propos/a-propos.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 export const routes: Routes = [{
-    path: 'about',
+    path: 'a-propos',
     component: AProposComponent
+}, {
+    path: 'contact',
+    component: ContactComponent
 }];
 
