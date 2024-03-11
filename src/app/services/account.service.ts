1c9b9e6fcc62253a0b98cb238f9a705d9d22c226
audre <audrey.kindermans@outlook.be>
2024-03-11 12:26:16 +0100

    creation account.service.ts

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export interface Account {
+  id: string;
+  title: string;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'http://localhost:8081/account/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAccountById(id: string) {
+    return this.httpClient.get<Account>(`${this.baseUrl}${id}`);
+  }
+
+  insertAccount(account: Account) {
+    return this.httpClient.post<Account>(this.baseUrl, account);
+  }
+
+  updateAccount(id: string, account: Partial<Account>) {
+    return this.httpClient.patch<Account>(`${this.baseUrl}${id}`, account);
+  }
+
+  deleteAccount(id: string) {
+    return this.httpClient.delete(`${this.baseUrl}${id}`);
+  }
+}
