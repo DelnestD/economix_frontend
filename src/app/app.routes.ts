db3df02299f3700bfe2745515a9b1576f7d7d75c
memol <memola.bruno@gmail.com>
2024-03-11 13:25:32 +0100

    derniere modifs a propos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
 import { AProposComponent } from './components/pages/a-propos/a-propos.component';
+// import { ContactComponent } from './components/pages/contact/contact.component';
+// import { BudgetPageComponent } from './components/pages/budget-page/budget-page.component';
 
-export const routes: Routes = [
-  {
-    path: 'about',
-    component: AProposComponent,
-  },
-];
+export const routes: Routes = [{
+    path: 'a-propos',
+    component: AProposComponent
+// }, {
+//     path: 'contact',
+//     component: ContactComponent
+// }, {
+//     path: 'budget',
+//     component: BudgetPageComponent
+}];
+
