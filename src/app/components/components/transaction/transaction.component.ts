bafb4c9a583b0d3b5877dc5b3db642bb8c46f08c
audre <audrey.kindermans@outlook.be>
2024-03-11 16:21:41 +0100

    dans app.config.ts, ajout de importProvidersFrom(HttpClientModule)

--- src/app/components/components/transaction/transaction.component.ts
+++ src/app/components/components/transaction/transaction.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css'
 })
