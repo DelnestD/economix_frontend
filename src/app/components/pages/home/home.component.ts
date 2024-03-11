1c01958628a5c53e19bc8406f86d4f86b349f818
Furone <furone-u-s-a@hotmail.com>
2024-03-11 09:47:11 +0100

    login

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    const loginForm = this.el.nativeElement.querySelector('#loginForm') as HTMLFormElement;
+
+    loginForm.addEventListener('submit', (event) => {
+      const emailInput = loginForm.elements['email'] as HTMLInputElement;
+      if (!this.validateEmail(emailInput.value)) {
+        alert('Veuillez entrer une adresse email valide pour le nom d\'utilisateur.');
+        event.preventDefault(); // Empêche l'envoi du formulaire si la validation échoue
+      }
+    });
+  }
+
+  private validateEmail(email: string): boolean {
+    // Expression régulière pour valider une adresse email simple
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 }
