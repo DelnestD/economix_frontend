bb67964b87791a5baae1ae3a9038ac32a217bc17
audre <audrey.kindermans@outlook.be>
2024-03-11 09:33:51 +0100

    avancement dans les services des transactions

--- src/app/components/components/budget-structure/budget-structure.component.ts
+++ src/app/components/components/budget-structure/budget-structure.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TransactionComponent } from '../transaction/transaction.component';
+import { Transaction, TransactionService } from '../../../services/transaction.service';
 
 @Component({
   selector: 'app-budget-structure',
@@ -8,7 +9,21 @@ import { TransactionComponent } from '../transaction/transaction.component';
   templateUrl: './budget-structure.component.html',
   styleUrl: './budget-structure.component.css'
 })
-export class BudgetStructureComponent {
+export class BudgetStructureComponent implements OnInit {
   totalBudget: number = 0;
   titleBudget: string = "Budget 1";
+
+  transactions: Transaction[] = [];
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions;
+  }
+  
+  loadTransactions() {
+    this.transactionService.getTransactionByBudgetId('1').subscribe(transactions => {
+      this.transactions = transactions;
+    })
+  }
 }
--- src/app/components/pages/budget-page/budget-page.component.ts
+++ src/app/components/pages/budget-page/budget-page.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BudgetStructureComponent } from '../../components/budget-structure/budget-structure.component';
 import { TransactionComponent } from '../../components/transaction/transaction.component';
 import { TransactionFormComponent } from '../../forms/transaction-form/transaction-form.component';
+import { Transaction, TransactionService } from '../../../services/transaction.service';
 
 @Component({
   selector: 'app-budget-page',
@@ -10,8 +11,22 @@ import { TransactionFormComponent } from '../../forms/transaction-form/transacti
   templateUrl: './budget-page.component.html',
   styleUrl: './budget-page.component.css'
 })
-export class BudgetPageComponent {
+export class BudgetPageComponent implements OnInit {
   totalCompte: number = 0;
+  
+  transactions: Transaction[] = [];
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions;
+  }
+  
+  loadTransactions() {
+    this.transactionService.getTransactionByAccountId('1').subscribe(transactions => {
+      this.transactions = transactions;
+    })
+  }
 
   showFormTransaction() {
     
