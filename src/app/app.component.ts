a5b34cbdf77551eb951f4f5b7ac8ad75b8ec0792
DelnestD <delnest.d@gmail.com>
2024-03-12 12:35:08 +0100

    change name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { FooterComponent } from './components/components/footer/footer.component
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = '0';
+  title = 'EconomiX';
 }
