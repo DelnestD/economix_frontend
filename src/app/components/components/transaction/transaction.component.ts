534c7f12ebd480766c3532007df61010c72f2743
audre <audrey.kindermans@outlook.be>
2024-03-13 09:09:46 +0100

    avancement dans page budget avec transactions dans compte

--- src/app/components/components/transaction/transaction.component.ts
+++ src/app/components/components/transaction/transaction.component.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
+  imports: [HttpClientModule],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css'
 })
--- src/app/components/pages/budget-page/budget-page.component.ts
+++ src/app/components/pages/budget-page/budget-page.component.ts
@@ -3,11 +3,12 @@ import { BudgetStructureComponent } from '../../components/budget-structure/budg
 import { TransactionComponent } from '../../components/transaction/transaction.component';
 import { TransactionFormComponent } from '../../forms/transaction-form/transaction-form.component';
 import { Transaction, TransactionService } from '../../../services/transaction.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-budget-page',
   standalone: true,
-  imports: [BudgetStructureComponent, TransactionComponent, TransactionFormComponent],
+  imports: [BudgetStructureComponent, TransactionComponent, TransactionFormComponent, HttpClientModule],
   templateUrl: './budget-page.component.html',
   styleUrl: './budget-page.component.css'
 })
@@ -19,16 +20,17 @@ export class BudgetPageComponent implements OnInit {
   constructor(private transactionService: TransactionService) {}
 
   ngOnInit(): void {
-    this.loadTransactions;
+    this.loadTransactions();
   }
   
   loadTransactions() {
-    this.transactionService.getTransactionByAccountId('1').subscribe(transactions => {
-      this.transactions = transactions;
+    this.transactionService.getTransactionByAccountId('3996620d-b6cf-4cf8-a03a-684a1940e784').subscribe(transaction => {
+      this.transactions = transaction;
+      console.log(this.transactions);
     })
   }
 
   showFormTransaction() {
-    
+
   }
 }
