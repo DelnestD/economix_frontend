d4154b56477f5ba6f17e378b095326a57ce3da36
memol <memola.bruno@gmail.com>
2024-03-14 16:14:05 +0100

    avancement changement de mot de passe

--- src/app/components/pages/parametres/mot-de-passe/mot-de-passe.component.ts
+++ src/app/components/pages/parametres/mot-de-passe/mot-de-passe.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ParametresComponent } from '../parametres.component';
+import { UserService } from '../../../../services/user.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mot-de-passe',
@@ -9,5 +11,23 @@ import { ParametresComponent } from '../parametres.component';
   styleUrl: './mot-de-passe.component.css'
 })
 export class MotDePasseComponent {
+  modifyPasswordForm = new FormGroup({
+    field: new FormControl("",[]),
+    confirm: new FormControl("",[])
+  }, confirmPassword);
 
+  txtErrorPassword: string = "";
+
+  constructor(private userService:UserService){ }
+
+  submit(){
+    console.log(this.modifyPasswordForm);
+    
+  }
+
+  modifyPassword(actualPassword: string, newPassword: string, confirmPassword: string){
+    if(newPassword. !== confirmPassword){
+        this.txtErrorPassword = "Les mots de passe ne correspondent pas.";
+    }
+  }
 }
