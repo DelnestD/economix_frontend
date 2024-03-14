8df94645e7463cc220a08b6db7dd5f17c34eac73
DelnestD <delnest.d@gmail.com>
2024-03-14 10:51:03 +0100

    update from cookie service

--- src/app/components/components/navbar/navbar.component.ts
+++ src/app/components/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +13,31 @@ export class NavbarComponent {
   isConnected: boolean = false;
   isRegistered: boolean = true;
 
-  constructor() {}
+  constructor(private cookieService: CookieService) {}
 
   ngOnInit() {
     //set here to check if user is connected
+    const tokenCookie = this.cookieService.get('accessToken');
+    const expirationCookie = new Date(this.cookieService.get('Expires'));
+
+    console.log('tokenCookie', tokenCookie);
+    console.log('expirationCookie', expirationCookie);
+
+    if (tokenCookie && !this.checkExpiration(expirationCookie)) {
+      this.isConnected = true;
+    } else {
+      this.isConnected = false;
+      this.isRegistered = true;
+    }
   }
 
+  checkExpiration(dateToCheck: Date) {
+    const dateNow = new Date();
+    if (dateNow > dateToCheck) {
+      return true;
+    }
+    return false;
+  }
   switchConnection() {
     this.isRegistered = !this.isRegistered;
   }
