10257d8fa90e8bae49fd5e2371225e914e74e76b
DelnestD <delnest.d@gmail.com>
2024-03-15 14:45:25 +0100

    update condition

--- src/app/components/validators/unique-email.validator.ts
+++ src/app/components/validators/unique-email.validator.ts
@@ -1,13 +1,15 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 export async function uniqueEmailValidator(
-  control: AbstractControl
+  control: AbstractControl,
+  userService: UserService
 ): Promise<ValidationErrors | null> {
   return new Promise((resolve) => {
     setTimeout(() => {
       const email = control.value;
       //TODO: call the user service to check if the email is unique
-      if (email === 'monemail@gmail.com') {
+      if (userService.getUserByEmail(email) !== null) {
         resolve({ uniqueEmail: true });
       }
       resolve(null);
