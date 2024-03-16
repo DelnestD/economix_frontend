957fff1579b13c4c171a61bbdd4702a0cc18da28
DelnestD <delnest.d@gmail.com>
2024-03-16 16:58:01 +0100

    remove unused component + uncomment some forms

--- src/app/components/pages/parametres/parametres.component.ts
+++ src/app/components/pages/parametres/parametres.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MotDePasseComponent } from './mot-de-passe/mot-de-passe.component';
 import { CreationGroupeComponent } from './creation-groupe/creation-groupe.component';
 import { MembresListeComponent } from './membres-liste/membres-liste.component';
 import { ModificationGroupeComponent } from './modification-groupe/modification-groupe.component';
@@ -8,10 +7,13 @@ import { UpdateUserComponent } from './update-user/update-user.component';
 @Component({
   selector: 'app-parametres',
   standalone: true,
-  imports: [MotDePasseComponent,CreationGroupeComponent,MembresListeComponent,ModificationGroupeComponent, UpdateUserComponent],
+  imports: [
+    CreationGroupeComponent,
+    MembresListeComponent,
+    ModificationGroupeComponent,
+    UpdateUserComponent,
+  ],
   templateUrl: './parametres.component.html',
-  styleUrl: './parametres.component.css'
+  styleUrl: './parametres.component.css',
 })
-export class ParametresComponent {
-
-}
+export class ParametresComponent {}
